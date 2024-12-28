d070622f0092fdd907b9acbc40d767a888d68fd4
jesieldotdev <jesiel364@gmail.com>
2024-12-28 11:25:32 -0400

    change: favicon, title

--- src/app/components/ChatHeader.tsx
+++ src/app/components/ChatHeader.tsx
@@ -1,19 +1,19 @@
 // components/ChatHeader.js
 
 import Image from "next/image";
-
+import Logo from '../logo.png'
 export default function ChatHeader() {
   return (
     <header className="flex items-center bg-white text-zinc-800  w-full p-2  shadow-md ">
       <Image
-        src="https://cdn-icons-png.flaticon.com/512/185/185988.png"
+        src={Logo}
         alt="avatar"
         width={40}
         height={40}
         className="rounded-full ml-2"
       />
       <div className="ml-2">
-        <p className="text-lg font-medium">LinkZap</p>
+        <p className="text-lg font-medium">Link para WhatsApp</p>
         <p className="text-sm font-light">Desenvolvido pela <span className="font-medium">JesielLabs</span></p>
       </div>
     </header>
